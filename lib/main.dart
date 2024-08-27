06b3621392ad913c7a68365b918629decc2d6817
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-08-27 22:43:55 +0800

    finish setting up the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Text('AutoCare', textDirection: TextDirection.ltr,));
   //sample connection to discord
 }
+
