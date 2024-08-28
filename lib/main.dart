29024930f3745e8e6638716ae63bcb8fb40d8bf3
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-08-28 14:57:20 +0800

    edited the main dart file to run the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Authentication/Screen/login.dart';
 
-
-void main() async {
+Future<void> main() async {
+  // Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(Text('AutoCare', textDirection: TextDirection.ltr,));
-  //sample connection to discord
+  // Run your app
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
