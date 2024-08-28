362c48531b963dd7eae448c49976ed131b59c6da
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-08-28 14:57:03 +0800

    added simple home screen for testing purposes

---
+++ lib/Authentication/Screen/homeScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'package:autocare_carowners/Authentication/Widgets/button.dart';
+import 'package:autocare_carowners/Authentication/Services/authentication.dart';
+import 'login.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Congratulation\nYou have successfully Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            MyButtons(
+              onTap: () async {
+                await AuthenticationMethod().signOut();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              text: "Log Out",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
