1fd332cff4d339e7268a64db8365a74d6dec2f6b
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-08-29 14:45:18 +0800

    update: navbar

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,4 +1,6 @@
+import 'package:autocare_carowners/screens/car_owner_car_profile.dart';
 import 'package:autocare_carowners/screens/car_owner_directory.dart';
+import 'package:autocare_carowners/screens/car_owner_edit_profile.dart';
 import 'package:autocare_carowners/screens/car_owner_message.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +25,8 @@ class _NavBarState extends State<NavBar> {
     CarOwnerMessage(),
     CarOwnerDirectory(),
     CarOwnerProfile(),
+    CarOwnerEditProfile(),
+    CarOwnerCarProfile(),
 
 
   ];
@@ -35,20 +39,24 @@ class _NavBarState extends State<NavBar> {
           backgroundColor: Colors.grey.shade300,
           key: _bottomNavigationKey,
           items: <Widget>[
-            Icon(Icons.home, size: 30),
-            Icon(Icons.message, size: 30),
-            Icon(Icons.directions_car, size: 30),
-            Icon(Icons.person, size: 30),
+            Icon(Icons.home, size: 30, color: _page == 0 ? Colors.orange : Colors.black),
+            Icon(Icons.message, size: 30, color: _page == 1 ? Colors.orange : Colors.black),
+            Icon(Icons.directions_car, size: 30, color: _page == 2 ? Colors.orange : Colors.black),
+            Icon(Icons.person, size: 30, color: _page == 3 ? Colors.orange : Colors.black),
+
+
+
           ],
           onTap: (index) {
             setState(() {
               _page = index;
             });
           },
+          index: _page, // Ensure the selected item is highlighted
         ),
-      body:
-      _screens[_page], // This will display the selected screen
+      body: _screens[_page], // Display the selected screen
     );
   }
 }
+
 
