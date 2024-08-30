5b7a2a4c913e979ed8df4a03ec3819044ac6b9e5
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-08-31 02:03:05 +0800

    update: edit profile page

--- lib/screens/car_owner_edit_profile.dart
+++ lib/screens/car_owner_edit_profile.dart
@@ -1,3 +1,4 @@
+import 'package:autocare_carowners/screens/car_owner_profile.dart';
 import 'package:flutter/material.dart';
 
 class CarOwnerEditProfile extends StatefulWidget {
@@ -20,7 +21,7 @@ class _CarOwnerEditProfileState extends State<CarOwnerEditProfile> {
       appBar: AppBar(
         title: Text(
           'EDIT PROFILE',
-          style: TextStyle(),
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.grey.shade300,
         actions: [
@@ -33,61 +34,109 @@ class _CarOwnerEditProfileState extends State<CarOwnerEditProfile> {
               )),
         ],
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(180),
-                child: Image.asset(
-                  'assets/images/profilePhoto.jpg',
-                  width: 360,
-                  height: 360,
-                ),
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Center(
-                  child: Text(
-                    '${profileName}',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
                     ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(320),
+                      child: Icon(
+                        Icons.person,
+                        size: 260,
+                        color: Colors.black12,
+        
+                      ),
+        
+                    ),
+        
                   ),
                 ),
-              ),
+                Positioned(
+                  right: 100,
+                  top: 190,
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                    child: ClipRRect(
+                      //borderRadius: BorderRadius.circular(5),
+                      child: IconButton(
+                          onPressed: () => {
 
-              Text(
-                '${emailAddress}',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black54,
+                          },
+                          icon: Icon(
+                            Icons.camera_alt_outlined,
+                            size: 40,
+                          )),
+        
+                    ),
+        
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+
+        
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                          hintText: 'Enter name'
+                        ),
+
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Email'
+                          ),
+
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Address'
+                        ),
+
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, left: 20, bottom: 50 ),
-                child: Row(
-                  children: [
-                    Icon(Icons.location_on_outlined, size: 30,),
-                    Text('${location}', style: TextStyle(color: Colors.black54, fontSize: 20),)
-                  ],
+
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)), minimumSize: Size(400, 50), backgroundColor: Colors.grey,),
+                  onPressed: () {
+                    Navigator.push(context,
+                        //pushReplacement if you don't want to go back
+                        MaterialPageRoute(builder: (context) => CarOwnerProfile()));
+                  },
+                  child: Text('SAVE', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
                 ),
-              ),
-
-            ],
-          ),
-
-        ],
+                
+              ],
+            ),
+        
+          ],
+        ),
       ),
     );
   }
