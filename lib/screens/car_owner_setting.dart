5e1552f679eca5c4d0125457908624e7635fd2c8
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-08-31 02:04:18 +0800

    update: settings app bar and navigation

--- lib/screens/car_owner_setting.dart
+++ lib/screens/car_owner_setting.dart
@@ -1,3 +1,4 @@
+import 'package:autocare_carowners/screens/car_owner_change_password.dart';
 import 'package:flutter/material.dart';
 
 class CarOwnerSetting extends StatefulWidget {
@@ -12,7 +13,22 @@ class _CarOwnerSettingState extends State<CarOwnerSetting> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(title: Text('SETTINGS'), backgroundColor: Colors.grey.shade300,),
+      appBar: AppBar(title: Text('SETTINGS', style: TextStyle(fontWeight: FontWeight.bold),), backgroundColor: Colors.grey.shade300,),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)), minimumSize: Size(400, 50), backgroundColor: Colors.grey, ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => CarOwnerChangePassword()));
+                },
+                child: Text('Change Password', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
