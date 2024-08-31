c56adeabdfe3074c1b169be5d493ac19527bb651
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-01 05:20:16 +0800

    edited: edited the homeScreen.dart file

--- lib/Authentication/screens/homeScreen.dart
+++ lib/Authentication/screens/homeScreen.dart
@@ -1,3 +1,5 @@
+import 'package:autocare_carowners/Authentication/Widgets/snackBar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:autocare_carowners/Authentication/Widgets/button.dart';
@@ -5,26 +7,53 @@ import 'package:autocare_carowners/Authentication/Services/authentication.dart';
 import 'login.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, this.child});
+
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
+
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('HOME'),
+      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              "Congratulations\nYou have successfully Login",
+              "Congratulations\nYou have successfully logged in",
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
+
+            const SizedBox(height: 16),
+            const Text(
+              "Signed In as",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+
+            const SizedBox(height: 8),
+            Text(
+              user != null ? user.email! : "No email available",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+
+            const SizedBox(height: 24),
             MyButtons(
               onTap: () async {
-                await AuthenticationMethod().signOut();
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                );
+                try {
+                  await AuthenticationMethod().signOut();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                } catch (e) {
+                 Utils.showSnackBar('Error Signing Out: $e');
+                }
               },
               text: "Log Out",
             ),
