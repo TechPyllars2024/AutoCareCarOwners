3851c2939b2ca428ab6fd1ec39ac8ae066dd8ebb
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-01 05:15:55 +0800

    updated: authentication.dart file for the services. FINAL

--- lib/Authentication/services/authentication.dart
+++ lib/Authentication/services/authentication.dart
@@ -2,47 +2,41 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../Widgets/snackBar.dart';
+
 class AuthenticationMethod {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
   // SIGN UP with Email and Password
   Future<String> signupUser({
+    required String name,
     required String email,
     required String password,
-    required String name,
   }) async {
-    String res = "Unfortunately, some errors occurred";
-    try {
-      if (email.isNotEmpty || password.isNotEmpty || name.isNotEmpty) {
-        //register the user with email and password
-        UserCredential credential = await _auth.createUserWithEmailAndPassword(
-            email: email, password: password);
-        //add the user to firestore database
-        print(credential.user!.uid);
-        await _firestore
-            .collection("users")
-            .doc(credential.user!.uid)
-            .set({'name': name, 'uid': credential.user!.uid, 'email': email});
-        res = 'Success!';
-      } else {
-        res = "Please fill in all fields.";
-      }
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'email-already-in-use') {
-        res = 'The account already exists for that email.';
-      } else if (e.code == 'invalid-email') {
-        res = 'The email address is badly formatted.';
-      } else if (e.code == 'weak-password') {
-        res = 'The password provided is too weak.';
-      } else {
-        res = e.message ?? "An unknown error occurred.";
-      }
-    } catch (err) {
-      return err.toString();
+    if (email.isEmpty || password.isEmpty || name.isEmpty) {
+      return "Please provide all the fields";
     }
-    return res;
+    try {
+      // Register the user with email and password
+      UserCredential credential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Add the user to Firestore database
+      await firestore.collection("users").doc(credential.user!.uid).set({
+        'name': name,
+        'uid': credential.user!.uid,
+        'email': email,
+      });
+      return 'Success!';
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? "An error occurred";
+    } catch (e) {
+      return "An unexpected error occurred";
+    }
   }
 
   // LOG IN with Email and Password
@@ -50,62 +44,86 @@ class AuthenticationMethod {
     required String email,
     required String password,
   }) async {
-    String res = "Unfortunately, some errors occurred";
-    try {
-      if (email.isNotEmpty || password.isNotEmpty) {
-        //logging in user with email and password
-        await _auth.signInWithEmailAndPassword(
-            email: email, password: password);
-        res = "SUCCESS";
-      } else {
-        res = "Please enter all the fields";
-      }
-    } catch (err) {
-      return err.toString();
+    if (email.isEmpty || password.isEmpty) {
+      return "Please provide all the fields";
     }
-    return res;
+
+    try {
+      // Logging in user with email and password
+      await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return "SUCCESS";
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? "An error occurred";
+    } catch (e) {
+      return "An unexpected error occurred";
+    }
   }
 
-//SIGN OUT
+  // SIGN OUT
   Future<void> signOut() async {
-    await _auth.signOut();
+    try {
+      await auth.signOut();
+      await googleSignIn.signOut();
+    } catch (e) {
+      Utils.showSnackBar("An error occurred while signing out");
+    }
   }
 
-  // SIGN IN WITH GOOGLE
-  Future<String> signInWithGoogle() async {
-    String res = "Unfortunately, some errors occurred";
-    try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-      if (googleUser != null) {
-        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-        final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken,
-          idToken: googleAuth.idToken,
-        );
-
-        UserCredential userCredential = await _auth.signInWithCredential(credential);
-
-        // Check if the user already exists in the Firestore database
-        DocumentSnapshot userDoc = await _firestore.collection("carowners").doc(userCredential.user!.uid).get();
-
-        if (!userDoc.exists) {
-          // If the user doesn't exist in the database, create a new entry
-          await _firestore.collection("carowners").doc(userCredential.user!.uid).set({
-            'name': userCredential.user!.displayName,
-            'uid': userCredential.user!.uid,
-            'email': userCredential.user!.email,
-          });
-        }
-
-        res = "SUCCESS";
-      } else {
-        res = "Google sign-in aborted";
-      }
-    } catch (err) {
-      return err.toString();
+  // RESET PASSWORD
+  Future<String> resetPassword({
+    required String email,
+  }) async {
+    if (email.isEmpty) {
+      return "Please provide an email";
     }
-    return res;
+
+    try {
+      await auth.sendPasswordResetEmail(email: email);
+      return "SUCCESS";
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? "An error occurred";
+    } catch (e) {
+      return "An unexpected error occurred";
+    }
+  }
+
+  // SIGN IN/LOG IN WITH GOOGLE
+  Future<String> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+      if (googleUser == null) {
+        return "Google sign-in aborted";
+      }
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential = await auth.signInWithCredential(credential);
+
+      DocumentSnapshot userDoc = await firestore.collection("users").doc(userCredential.user!.uid).get();
+
+      if (!userDoc.exists) {
+        await firestore.collection("users").doc(userCredential.user!.uid).set({
+          'name': userCredential.user!.displayName ?? "No Name",
+          'uid': userCredential.user!.uid,
+          'email': userCredential.user!.email ?? "No Email",
+        });
+      }
+
+      return "SUCCESS";
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? "An error occurred";
+    } catch (e) {
+      return "An unexpected error occurred";
+    }
   }
 }
