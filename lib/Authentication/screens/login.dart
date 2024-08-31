329787e54e551c28faca3fbdbe30d15bd6101e60
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-01 05:21:23 +0800

    updated: edited the login.dart file mainly on functionality

--- lib/Authentication/screens/login.dart
+++ lib/Authentication/screens/login.dart
@@ -1,19 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:autocare_carowners/Authentication/Screen/homeScreen.dart';
-import 'package:autocare_carowners/Authentication/Screen/signup.dart';
+
+import 'package:autocare_carowners/Authentication/screens/forgotPassword.dart';
+import 'package:autocare_carowners/Authentication/screens/homeScreen.dart';
+import 'package:autocare_carowners/Authentication/screens/signup.dart';
 import 'package:autocare_carowners/Authentication/Services/authentication.dart';
 import 'package:autocare_carowners/Authentication/Widgets/snackBar.dart';
 import '../Widgets/button.dart';
 import '../Widgets/text_field.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({
+    super.key,
+    this.child
+  });
+
+  final Widget? child;
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
@@ -25,116 +34,175 @@ class _LoginScreenState extends State<LoginScreen> {
     passwordController.dispose();
   }
 
-  // Email and password auth part
+  // Handles email and password authentication
   void loginUser() async {
     setState(() {
       isLoading = true;
     });
-    // Login user using our auth method
-    String res = await AuthenticationMethod().loginUser(
-        email: emailController.text, password: passwordController.text);
 
-    setState(() {
-      isLoading = false;
-    });
+    String res = await AuthenticationMethod().loginUser(
+      email: emailController.text,
+      password: passwordController.text,
+    );
 
     if (res == "SUCCESS") {
-      // Navigate to the home screen
+      // Check if email is verified
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null && user.emailVerified) {
+        // Navigate to the home screen if email is verified
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      } else {
+        // Inform the user to verify their email
+        setState(() {
+          isLoading = false;
+        });
+        Utils.showSnackBar("Please verify your email address.");
+      }
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+      Utils.showSnackBar(res);
+    }
+  }
+
+  // Handles Google authentication
+  Future<void> logInWithGoogle() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    String res = await AuthenticationMethod().signInWithGoogle();
+
+    if (res == "SUCCESS") {
+      setState(() {
+        isLoading = false;
+      });
+
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (context) => const HomeScreen(),
         ),
       );
     } else {
-      // Show error
-      showSnackBar(context, res);
+      setState(() {
+        isLoading = false;
+      });
+      Utils.showSnackBar(res);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : SizedBox(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Your logo or any other branding here
-              TextFieldInput(
+        child: Center(
+          child: SizedBox(
+            width: size.width * 0.9,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Add logo or image here
+                if (isLoading)
+                  const Center(child: CircularProgressIndicator()),
+                TextFieldInput(
                   icon: Icons.email,
                   textEditingController: emailController,
                   hintText: 'Enter your email',
-                  textInputType: TextInputType.text),
-              TextFieldInput(
-                icon: Icons.lock,
-                textEditingController: passwordController,
-                hintText: 'Enter your password',
-                textInputType: TextInputType.text,
-                isPass: true,
-              ),
-              MyButtons(onTap: loginUser, text: "Log In"),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(height: 1, color: Colors.black26),
-                  ),
-                  const Text("  or  "),
-                  Expanded(
-                    child: Container(height: 1, color: Colors.black26),
-                  )
-                ],
-              ),
-              // Don't have an account? Go to signup screen
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 100),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don't have an account? "),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                            const SignupScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        "SignUp",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                  textInputType: TextInputType.emailAddress,
+                ),
+                TextFieldInput(
+                  icon: Icons.lock,
+                  textEditingController: passwordController,
+                  hintText: 'Enter your password',
+                  textInputType: TextInputType.text,
+                  isPass: true,
+                ),
+                MyButtons(onTap: loginUser, text: "Log In"),
+                SizedBox(height: size.height * 0.03),
+                const Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
                       ),
-                    )
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        'OR',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
                   ],
                 ),
-              ),
-            ],
+                MyButtons(onTap: logInWithGoogle, text: "Log In with Google"),
+                const SizedBox(height: 24),
+                GestureDetector(
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.blue,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ForgotPasswordScreen(),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(height: 1, color: Colors.black26),
+                    ),
+                    const Text("  or  "),
+                    Expanded(
+                      child: Container(height: 1, color: Colors.black26),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 100),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account? "),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const SignupScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
-
-  Container socialIcon(image) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 32,
-        vertical: 15,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFFedf0f8),
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          color: Colors.black45,
-          width: 2,
-        ),
-      ),
-      child: Image.network(
-        image,
-        height: 40,
       ),
     );
   }
