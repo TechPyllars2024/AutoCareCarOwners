f88b3c14f20df3772f748bc48a60485884fe7e46
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-01 05:17:32 +0800

    updated: edited the button.dart for color and padding parameters

--- lib/Authentication/widgets/button.dart
+++ lib/Authentication/widgets/button.dart
@@ -3,28 +3,35 @@ import 'package:flutter/material.dart';
 class MyButtons extends StatelessWidget {
   final VoidCallback onTap;
   final String text;
+  final Color color;
+  final double padding;
+
   const MyButtons({
     super.key,
     required this.onTap,
     required this.text,
+    this.color = Colors.blue,
+    this.padding = 20.0,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
+      splashColor: Colors.black26,
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(padding),
         child: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.symmetric(vertical: 12),
-          decoration: const ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
-                ),
+          decoration: ShapeDecoration(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
               ),
-              color: Colors.green),
+            ),
+            color: color,
+          ),
           child: Text(
             text,
             style: const TextStyle(
