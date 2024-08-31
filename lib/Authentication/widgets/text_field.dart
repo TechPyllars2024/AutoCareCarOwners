59fa12a859ae34b7a29a30e32f040422fc14d608
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-01 05:23:54 +0800

    updated: edited the text field file

--- lib/Authentication/widgets/text_field.dart
+++ lib/Authentication/widgets/text_field.dart
@@ -6,6 +6,8 @@ class TextFieldInput extends StatelessWidget {
   final String hintText;
   final IconData? icon;
   final TextInputType textInputType;
+  final Widget? child;
+
   const TextFieldInput({
     super.key,
     required this.textEditingController,
@@ -13,6 +15,7 @@ class TextFieldInput extends StatelessWidget {
     required this.hintText,
     this.icon,
     required this.textInputType,
+    this.child,
   });
 
   @override
@@ -20,24 +23,27 @@ class TextFieldInput extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
       child: TextField(
-          style: const TextStyle(fontSize: 20),
-          controller: textEditingController,
-          decoration: InputDecoration(
-              labelText: hintText,
-              prefixIcon: Icon(icon, color: Colors.black45, size: 18),
-              labelStyle: const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-              focusedBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-              ),
-          keyboardType: textInputType,
-          obscureText: isPass),
+        style: const TextStyle(fontSize: 20),
+        controller: textEditingController,
+        decoration: InputDecoration(
+          labelText: hintText,
+          prefixIcon: icon != null
+              ? Icon(icon, color: Colors.black45, size: 24)
+              : null,
+          labelStyle: const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+        keyboardType: textInputType,
+        obscureText: isPass,
+      ),
     );
   }
 }
