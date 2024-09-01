d422a3e1c1aa37b739922a3bff5cda44498bfc62
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-02 02:58:07 +0800

    updated: fixed the error

--- lib/Authentication/screens/forgotPassword.dart
+++ lib/Authentication/screens/forgotPassword.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:autocare_carowners/Authentication/screens/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:autocare_carowners/Authentication/Services/authentication.dart';
