7ee4fbabcf5867d6fdb0fdcedd3a235340de108f
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-01 18:19:53 +0800

    update: main fix paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:autocare_carowners/widgets/navbar.dart';
+import 'package:autocare_carowners/ProfileManagement/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
