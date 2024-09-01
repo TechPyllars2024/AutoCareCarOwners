bea245e04eb2053407edcbe75563d9b194739333
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-01 17:55:25 +0800

    fixed button

--- lib/Authentication/widgets/button.dart
+++ lib/Authentication/widgets/button.dart
@@ -3,35 +3,30 @@ import 'package:flutter/material.dart';
 class MyButtons extends StatelessWidget {
   final VoidCallback onTap;
   final String text;
-  final Color color;
-  final double padding;
-
+  
   const MyButtons({
     super.key,
     required this.onTap,
     required this.text,
-    this.color = Colors.blue,
-    this.padding = 20.0,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      splashColor: Colors.black26,
       child: Padding(
-        padding: EdgeInsets.all(padding),
+        padding: const EdgeInsets.all(20),
         child: Container(
           alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(vertical: 12),
-          decoration: ShapeDecoration(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(30),
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          width: 300,
+          decoration: const ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
               ),
-            ),
-            color: color,
-          ),
+              color: Colors.green),
           child: Text(
             text,
             style: const TextStyle(
