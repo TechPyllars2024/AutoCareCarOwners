a23e1a05fc9b448aa004c1c2d80381ab0ee4bad8
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-03 17:21:16 +0800

    update: login image

--- lib/Authentication/widgets/carImage.dart
+++ lib/Authentication/widgets/carImage.dart
@@ -12,7 +12,7 @@ class CarImageWidget extends StatelessWidget {
 Widget build(BuildContext context) {
     return Image.asset(
       imagePath,
-      height: 180,
+      height: 280,
     );
   }
 }
