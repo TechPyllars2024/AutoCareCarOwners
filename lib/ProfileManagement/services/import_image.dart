a1bd4f8c5195c25e84d680251c810384d22e0583
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-07 20:21:34 +0800

    Added file for Import Image

---
+++ lib/ProfileManagement/services/import_image.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImportImage extends StatelessWidget {
+  const ImportImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Import Image'),
+      ),
+    );
+  }
+}
