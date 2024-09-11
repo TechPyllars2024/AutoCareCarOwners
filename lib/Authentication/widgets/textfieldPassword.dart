7ce5b23ba8a435e7a2491a8e1ce69a31580b738d
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-12 03:01:07 +0800

    feat-auth: updated the text field input and added validator.dart

--- lib/Authentication/widgets/textfieldPassword.dart
+++ lib/Authentication/widgets/textfieldPassword.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class TextFieldInput extends StatefulWidget {
+class TextFieldPassword extends StatefulWidget {
   final TextEditingController textEditingController;
   final bool isPass;
   final String hintText;
   final IconData? icon;
   final TextInputType textInputType;
-  final String? Function(String?)? validator; // Add validator function
+  final String? Function(String?)? validator; // Password validator
 
-  const TextFieldInput({
+  const TextFieldPassword({
     super.key,
     required this.textEditingController,
     this.isPass = false,
@@ -19,10 +19,11 @@ class TextFieldInput extends StatefulWidget {
   });
 
   @override
-  _TextFieldInputState createState() => _TextFieldInputState();
+  _TextFieldPasswordState createState() => _TextFieldPasswordState();
 }
 
-class _TextFieldInputState extends State<TextFieldInput> {
+class _TextFieldPasswordState extends State<TextFieldPassword> {
+  bool _isPasswordVisible = false;
   String? errorMessage;
 
   @override
@@ -35,7 +36,8 @@ class _TextFieldInputState extends State<TextFieldInput> {
           TextField(
             style: const TextStyle(fontSize: 15),
             controller: widget.textEditingController,
-            obscureText: widget.isPass, // Handle password visibility
+            obscureText: widget.isPass &&
+                !_isPasswordVisible, // Toggle password visibility
             keyboardType: widget.textInputType,
             decoration: InputDecoration(
               labelText: widget.hintText,
@@ -44,6 +46,21 @@ class _TextFieldInputState extends State<TextFieldInput> {
                   : null,
               labelStyle:
                   const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
+              suffixIcon: widget.isPass
+                  ? IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    )
+                  : null,
               border: const OutlineInputBorder(
                 borderSide: BorderSide(color: Colors.black),
               ),
