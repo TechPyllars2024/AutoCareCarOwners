ee4d6db245c2312d64e80d6cf0e23f8fc852770e
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-12 03:00:23 +0800

    feat-auth: updated the sign up

--- lib/Authentication/screens/signup.dart
+++ lib/Authentication/screens/signup.dart
@@ -1,24 +1,23 @@
-import 'package:autocare_carowners/Authentication/screens/verifyEmail.dart';
-import 'package:autocare_carowners/Authentication/widgets/googleButton.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:autocare_carowners/Authentication/widgets/carImage.dart';
-import 'package:autocare_carowners/Authentication/widgets/or.dart';
-import 'package:autocare_carowners/Authentication/widgets/texfieldPassword.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:autocare_carowners/Authentication/Services/authentication.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+
 import '../Widgets/button.dart';
 import '../Widgets/snackBar.dart';
 import '../Widgets/text_field.dart';
+import '../widgets/textfieldPassword.dart';
+import '../widgets/validator.dart';
 import 'homeScreen.dart';
 import 'login.dart';
+import 'package:autocare_carowners/Authentication/widgets/carImage.dart';
+import 'package:autocare_carowners/Authentication/widgets/or.dart';
+import 'package:autocare_carowners/Authentication/Services/authentication.dart';
+import 'package:autocare_carowners/Authentication/screens/verifyEmail.dart';
+import 'package:autocare_carowners/Authentication/widgets/googleButton.dart';
 
 class SignupScreen extends StatefulWidget {
-  const SignupScreen({
-    super.key,
-    this.child
-  });
+  const SignupScreen({super.key, this.child});
 
   final Widget? child;
 
@@ -30,7 +29,8 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   bool isLoading = false;
 
   @override
@@ -56,7 +56,7 @@ class _SignupScreenState extends State<SignupScreen> {
       return;
     }
 
-    String res = await AuthenticationMethod().signupUser(
+    String res = await AuthenticationMethod().signupCarOwner(
       email: emailController.text,
       password: passwordController.text,
       name: nameController.text,
@@ -101,7 +101,7 @@ class _SignupScreenState extends State<SignupScreen> {
       isLoading = true;
     });
 
-    String res = await AuthenticationMethod().signInWithGoogle();
+    String res = await AuthenticationMethod().signInWithGoogleForCarOwner();
 
     if (res == "SUCCESS") {
       setState(() {
@@ -121,7 +121,7 @@ class _SignupScreenState extends State<SignupScreen> {
     }
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
@@ -150,7 +150,7 @@ class _SignupScreenState extends State<SignupScreen> {
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 50,
-                          color: Colors.orange, // Different color
+                          color: Colors.orange,
                         ),
                       ),
                     ],
@@ -159,32 +159,52 @@ class _SignupScreenState extends State<SignupScreen> {
               ),
 
               // Sign Up Image
-              const CarImageWidget(imagePath: 'lib/Authentication/assets/images/car.png').animate()
-                                                                                                .fadeIn(duration: const Duration(seconds: 1)),
+              const CarImageWidget(
+                      imagePath: 'lib/Authentication/assets/images/car.png')
+                  .animate()
+                  .fadeIn(duration: const Duration(seconds: 1)),
               // Sign Up Form
               Container(
                 padding: const EdgeInsets.all(16),
                 decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 child: Column(
-                  children: <Widget>[               
+                  children: <Widget>[
                     TextFieldInput(
-                        icon: Icons.person,
-                        textEditingController: nameController,
-                        hintText: 'Enter your Name',
-                        textInputType: TextInputType.text),
+                      icon: Icons.person,
+                      textEditingController: nameController,
+                      hintText: 'Enter your Name',
+                      textInputType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a name';
+                        }
+                        return null;
+                      },
+                    ),
                     TextFieldInput(
-                        icon: Icons.email,
-                        textEditingController: emailController,
-                        hintText: 'Enter your Email',
-                        textInputType: TextInputType.text),
+                      icon: Icons.email,
+                      textEditingController: emailController,
+                      hintText: 'Enter your Email',
+                      textInputType: TextInputType.text,
+                      validator: (value) {
+                        final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an email';
+                        } else if (!emailRegex.hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                    ),
                     TextFieldPassword(
                       icon: Icons.lock,
                       textEditingController: passwordController,
                       hintText: 'Enter your Password',
                       textInputType: TextInputType.text,
+                      validator: passwordValidator,
                       isPass: true,
                     ),
                     TextFieldPassword(
@@ -192,12 +212,18 @@ class _SignupScreenState extends State<SignupScreen> {
                       textEditingController: confirmPasswordController,
                       hintText: 'Confirm your Password',
                       textInputType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        }
+                        return null;
+                      },
                       isPass: true,
                     ),
-                    
+
                     // Sign Up Button
                     MyButtons(onTap: signupUser, text: "Sign Up"),
-                    
+
                     // Sign Up OR
                     const Or(),
 
@@ -207,7 +233,7 @@ class _SignupScreenState extends State<SignupScreen> {
                       onTap: signInWithGoogle,
                       hintText: 'Sign Up with Google',
                     ),
-                    
+
                     // Already have an account? Log In
                     const SizedBox(height: 30),
                     TextButton(
@@ -230,7 +256,9 @@ class _SignupScreenState extends State<SignupScreen> {
                                   // Navigate to LoginScreen
                                   Navigator.push(
                                     context,
-                                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginScreen()),
                                   );
                                 },
                             ),
@@ -240,11 +268,11 @@ class _SignupScreenState extends State<SignupScreen> {
                     ),
                   ],
                 ),
-              ).animate()
-               .slide(duration: const Duration(milliseconds: 500), 
-                      curve: Curves.easeInOut,
-                      begin: const Offset(0, 1), 
-                      end: const Offset(0, 0)),
+              ).animate().slide(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
+                  begin: const Offset(0, 1),
+                  end: const Offset(0, 0)),
             ],
           ),
         ),
