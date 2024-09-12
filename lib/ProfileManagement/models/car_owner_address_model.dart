9dcb5a4010f15a6838bf4685ac5f2430ea246781
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-12 19:24:57 +0800

    Delete unused code and files

--- lib/ProfileManagement/models/car_owner_address_model.dart
+++ lib/ProfileManagement/models/car_owner_address_model.dart
@@ -38,21 +38,3 @@ class CarOwnerAddressModel {
     );
   }
 }
-
-// class CarOwnerAddressModel {
-//   String fullName;
-//   String phoneNumber;
-//   String street;
-//   String city;
-//   String country;
-//   bool isDefault;
-
-//   CarOwnerAddressModel({
-//     required this.fullName,
-//     required this.phoneNumber,
-//     required this.street,
-//     required this.city,
-//     required this.country,
-//     this.isDefault = false,
-//   });
-// }
--- lib/ProfileManagement/models/car_owner_car_details_model.dart
+++ lib/ProfileManagement/models/car_owner_car_details_model.dart
@@ -39,20 +39,3 @@ class CarDetailsModel {
   }
 }
 
-// class CarDetailsModel {
-//   String brand;
-//   String model;
-//   int year;
-//   String color;
-//   String transmissionType;
-//   String fuelType;
-
-//   CarDetailsModel({
-//     required this.brand,
-//     required this.model,
-//     required this.year,
-//     required this.color,
-//     required this.transmissionType,
-//     required this.fuelType,
-//   });
-// }
