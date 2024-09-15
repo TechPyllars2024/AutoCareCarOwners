4d9fe4130bc5570ef962899063248cc9f3bdabdd
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-16 01:22:44 +0800

    update: textfield outlines when typing

--- lib/ServiceDirectory/widgets/text_field.dart
+++ lib/ServiceDirectory/widgets/text_field.dart
@@ -39,12 +39,12 @@ class TextFieldInput extends StatelessWidget {
             //     : null,
             labelStyle: const TextStyle(color: Color.fromARGB(255, 77, 76, 76)),
             border:  OutlineInputBorder(
-           //   borderSide: BorderSide(color: Colors.grey),
-           //   borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
             ),
             enabledBorder:  OutlineInputBorder(
-            //  borderSide: BorderSide(color: Colors.grey),
-            //  borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
             ),
             focusedBorder:  OutlineInputBorder(
               borderSide: BorderSide(color: Colors.grey),
