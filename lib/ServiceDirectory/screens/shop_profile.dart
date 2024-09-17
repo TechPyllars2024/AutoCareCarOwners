f2c405f94c6822a5e1eb35d1b616abcbdc0561ca
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-18 00:27:26 +0800

    update: shop profile list view

--- lib/ServiceDirectory/screens/shop_profile.dart
+++ lib/ServiceDirectory/screens/shop_profile.dart
@@ -4,24 +4,35 @@ class ShopProfile extends StatefulWidget {
   final String serviceName; // Accept the serviceName
   final String shopName; // Accept the shopName
 
-  const ShopProfile({super.key, required this.serviceName, required this.shopName});
+  //child: Text('Welcome to ${widget.shopName}'),
+
+  const ShopProfile(
+      {super.key, required this.serviceName, required this.shopName});
 
   @override
   State<ShopProfile> createState() => _ShopProfileState();
 }
 
 class _ShopProfileState extends State<ShopProfile> {
+  final double coverHeight = 220;
+  final double profileHeight = 130;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Keep the AppBar title as the serviceName
-        title: Text(widget.serviceName),
-      ),
-      body: Center(
-        // Display the shop details (here just the name for simplicity)
-        child: Text('Welcome to ${widget.shopName}'),
-      ),
-    );
+        appBar: AppBar(
+          // Keep the AppBar title as the serviceName
+          title: Text(widget.serviceName),
+        ),
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 8),
+                children: [
+
+                ],
+              ),
+        )));
   }
 }
