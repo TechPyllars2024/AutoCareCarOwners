954af91bd05f24d699519799ff96e3b49d5eea9c
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-18 14:24:27 +0800

    feat-Profile_Management: updated and fixed the car_owner_profile_model.dart

--- lib/ProfileManagement/models/car_owner_profile_model.dart
+++ lib/ProfileManagement/models/car_owner_profile_model.dart
@@ -1,32 +1,34 @@
 class CarOwnerProfileModel {
-  final String profileId;
   final String uid;
+  final String profileId;
   final String name;
   final String email;
   final String profileImage;
 
   CarOwnerProfileModel({
-    required this.profileId,
     required this.uid,
+    required this.profileId,
     required this.name,
     required this.email,
     required this.profileImage,
   });
 
+  // Factory method to create CarOwnerProfileModel from Firestore document
   factory CarOwnerProfileModel.fromDocument(Map<String, dynamic> doc, String uid) {
     return CarOwnerProfileModel(
       uid: uid,
-      profileId: doc['profileId'],
-      name: doc['name'] ?? '',
-      email: doc['email'] ?? '',
+      profileId: doc['profileId'] ?? '',
+      name: doc['name'] ?? 'Unnamed',
+      email: doc['email'] ?? 'No Email',
       profileImage: doc['profileImage'] ?? '',
     );
   }
 
+  // Method to convert CarOwnerProfileModel to a map (for saving to Firestore)
   Map<String, dynamic> toMap() {
     return {
-      'profileId': profileId,
       'uid': uid,
+      'profileId': profileId,
       'name': name,
       'email': email,
       'profileImage': profileImage,
