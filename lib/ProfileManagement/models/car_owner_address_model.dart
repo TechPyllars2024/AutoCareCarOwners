0e68547d8c3fab6b1f822f9bdf3596d61f271aaa
RafaelIIIPrudente <rflprdnt@gmail.com>
2024-09-18 14:23:12 +0800

    feat-Profile_Management: updated and fixed the car_owner_address_model.dart

--- lib/ProfileManagement/models/car_owner_address_model.dart
+++ lib/ProfileManagement/models/car_owner_address_model.dart
@@ -1,46 +1,42 @@
 class CarOwnerAddressModel {
-  String addressId;
-  String profileId;
   String fullName;
   String phoneNumber;
   String street;
+  String baranggay;
   String city;
-  String country;
+  String province;
   bool isDefault;
 
   CarOwnerAddressModel({
-    required this.addressId,
-    required this.profileId,
     required this.fullName,
     required this.phoneNumber,
     required this.street,
+    required this.baranggay,
     required this.city,
-    required this.country,
+    required this.province,
     this.isDefault = false,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'addressId': addressId,
-      'profileId': profileId,
       'fullName': fullName,
       'phoneNumber': phoneNumber,
       'street': street,
+      'baranggay': baranggay,
       'city': city,
-      'country': country,
+      'province': province,
       'isDefault': isDefault,
     };
   }
 
-  factory CarOwnerAddressModel.fromMap(Map<String, dynamic> map, String profileId) {
+  factory CarOwnerAddressModel.fromMap(Map<String, dynamic> map) {
     return CarOwnerAddressModel(
-      addressId: map['addressId'],
-      profileId: profileId,
       fullName: map['fullName'],
       phoneNumber: map['phoneNumber'],
       street: map['street'],
+      baranggay: map['baranggay'],
       city: map['city'],
-      country: map['country'],
+      province: map['province'],
       isDefault: map['isDefault'] ?? false,
     );
   }
