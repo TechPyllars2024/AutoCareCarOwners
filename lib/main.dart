1d1278ed77acaf16204386cde594dea58ba218f0
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-18 20:23:01 +0800

    update: remove imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:autocare_carowners/Authentication/screens/homeScreen.dart';
+
 import 'package:autocare_carowners/Authentication/screens/verifyEmail.dart';
-import 'package:autocare_carowners/ProfileManagement/screens/car_owner_profile.dart';
+
 import 'package:autocare_carowners/ProfileManagement/widgets/navbar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
