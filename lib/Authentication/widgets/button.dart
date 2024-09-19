0a9f8bf4dce9a7e648324a602e92e3fc48096cec
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-19 15:08:17 +0800

    update: added loading on the button

--- lib/Authentication/widgets/button.dart
+++ lib/Authentication/widgets/button.dart
@@ -1,39 +1,53 @@
 import 'package:flutter/material.dart';
 
 class MyButtons extends StatelessWidget {
-  final VoidCallback onTap;
+  final VoidCallback? onTap; // Allow for null callbacks
   final String text;
-  
+  final bool isLoading; // Add this parameter
+
   const MyButtons({
     super.key,
     required this.onTap,
     required this.text,
+    this.isLoading = false, // Default to false if not provided
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
+      onTap: isLoading ? null : onTap, // Disable tap when loading
       child: Padding(
         padding: const EdgeInsets.only(top: 0.0),
         child: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.symmetric(vertical: 10),
           width: double.infinity,
-          decoration: const ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
               ),
-              color: Colors.orange),
-          child: Text(
-            text,
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
             ),
+            color: Colors.orange, // Keep the button color consistent
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // Display text or loading indicator based on isLoading
+              if (isLoading)
+                const CircularProgressIndicator(
+                  color: Colors.white,
+                )
+              else
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ],
           ),
         ),
       ),
