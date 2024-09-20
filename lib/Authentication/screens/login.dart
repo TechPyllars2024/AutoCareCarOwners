e8823818ada3a23cf9efb7c92e934d841c81aef9
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-20 23:23:22 +0800

    update: adjusted the single scroll view

--- lib/Authentication/screens/login.dart
+++ lib/Authentication/screens/login.dart
@@ -36,7 +36,6 @@ class _LoginScreenState extends State<LoginScreen> {
     passwordController.dispose();
   }
 
-  // Handles email and password authentication
   void loginUser() async {
     setState(() {
       isLoading = true;
@@ -48,7 +47,6 @@ class _LoginScreenState extends State<LoginScreen> {
     );
 
     if (res == "SUCCESS") {
-      // Check if email is verified
       User? user = FirebaseAuth.instance.currentUser;
       if (user != null && user.emailVerified) {
         Navigator.of(context).pushReplacement(
@@ -70,7 +68,6 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  /// Handles Google Log-In in the UI for Car Owners
   Future<void> logInWithGoogleForCarOwners() async {
     setState(() {
       isLoading = true;
@@ -99,164 +96,163 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       resizeToAvoidBottomInset: false,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const CarImageWidget(
-              imagePath: 'lib/Authentication/assets/images/carBlack.png')
-              .animate()
-              .fadeIn(duration: const Duration(seconds: 2)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const CarImageWidget(
+                imagePath: 'lib/Authentication/assets/images/carBlack.png')
+                .animate()
+                .fadeIn(duration: const Duration(seconds: 2)),
 
-          // Expanded container that stretches to the bottom of the screen
-          Expanded(
-            child: Container(
+            // Main content container
+            Container(
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
               ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 12),
-                      child: RichText(
-                        text: const TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "Auto",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 50,
-                                color: Colors.black,
-                              ),
-                            ),
-                            TextSpan(
-                              text: "Care",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 50,
-                                color: Colors.orange,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ).animate().fadeIn(duration: const Duration(seconds: 3)),
-                    ),
-                    TextFieldInput(
-                      icon: Icons.email,
-                      textEditingController: emailController,
-                      hintText: 'Email',
-                      textInputType: TextInputType.text,
-                      validator: (value) {
-                        final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter an email';
-                        } else if (!emailRegex.hasMatch(value)) {
-                          return 'Please enter a valid email address';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFieldPassword(
-                      icon: Icons.lock,
-                      textEditingController: passwordController,
-                      hintText: 'Password',
-                      textInputType: TextInputType.text,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please confirm your password';
-                        }
-                        return null;
-                      },
-                      isPass: true,
-                    ),
-
-                    // Forgot Password
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        GestureDetector(
-                          child: const Text(
-                            'Forgot Password?',
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 12),
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Auto",
                             style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 50,
                               color: Colors.black,
-                              fontSize: 13,
                             ),
                           ),
-                          onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                              const ForgotPasswordScreen(),
+                          TextSpan(
+                            text: "Care",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 50,
+                              color: Colors.orange,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-
-                    // Sign Up Button
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: MyButtons(
-                        onTap: loginUser,
-                        text: "Log In",
-                        isLoading: isLoading, // Pass the loading state
+                        ],
                       ),
+                    ).animate().fadeIn(duration: const Duration(seconds: 3)),
+                  ),
+                  TextFieldInput(
+                    icon: Icons.email,
+                    textEditingController: emailController,
+                    hintText: 'Email',
+                    textInputType: TextInputType.text,
+                    validator: (value) {
+                      final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an email';
+                      } else if (!emailRegex.hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFieldPassword(
+                    icon: Icons.lock,
+                    textEditingController: passwordController,
+                    hintText: 'Password',
+                    textInputType: TextInputType.text,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your password';
+                      }
+                      return null;
+                    },
+                    isPass: true,
+                  ),
+
+                  // Forgot Password
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 13,
+                          ),
+                        ),
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                            const ForgotPasswordScreen(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Log In Button
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: MyButtons(
+                      onTap: loginUser,
+                      text: "Log In",
+                      isLoading: isLoading, // Pass the loading state
                     ),
+                  ),
 
-                    // Sign Up OR
-                    SizedBox(height: size.height * 0.02),
-                    const Or(),
+                  // OR Divider
+                  SizedBox(height: size.height * 0.02),
+                  const Or(),
 
-                    // Sign Up with Google
-                    SizedBox(height: size.height * 0.03),
-                    GoogleButton(
-                      onTap: logInWithGoogleForCarOwners, // Google button is always enabled
-                      hintText: 'Log In with Google',
-                    ),
+                  // Google Sign-In Button
+                  SizedBox(height: size.height * 0.03),
+                  GoogleButton(
+                    onTap: logInWithGoogleForCarOwners, // Google button is always enabled
+                    hintText: 'Log In with Google',
+                  ),
 
-                    // Already have an account? Log In
-                    const SizedBox(height: 50),
-                    TextButton(
-                      onPressed: () {
-                        // Handle navigation to login screen
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          text: "Don't have an account? ",
-                          style: const TextStyle(color: Colors.black),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Sign Up',
-                              style: const TextStyle(
-                                color: Colors.orange,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  // Navigate to SignupScreen
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                        const SignupScreen()),
-                                  );
-                                },
+                  // Already have an account? Sign Up
+                  const SizedBox(height: 50),
+                  TextButton(
+                    onPressed: () {
+                      // Handle navigation to login screen
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Don't have an account? ",
+                        style: const TextStyle(color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: const TextStyle(
+                              color: Colors.orange,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
-                        ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                      const SignupScreen()),
+                                );
+                              },
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ).animate().slide(
                 duration: const Duration(milliseconds: 1000),
                 curve: Curves.easeInOut,
                 begin: const Offset(0, 1),
                 end: const Offset(0, 0)),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
