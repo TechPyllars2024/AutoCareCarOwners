b29e1cacc1a4cd6194c6e617948725ecf1f3f3ee
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-09-22 19:29:29 +0800

    update: shop profile  appbar

--- lib/Service Directory Management/screens/shop_profile.dart
+++ lib/Service Directory Management/screens/shop_profile.dart
@@ -47,6 +47,7 @@ class _ShopProfileState extends State<ShopProfile> {
     return Scaffold(
         backgroundColor: Colors.grey.shade100,
         appBar: AppBar(
+          backgroundColor: Colors.grey.shade100,
           // Keep the AppBar title as the serviceName
           title: Text(widget.serviceName),
         ),
