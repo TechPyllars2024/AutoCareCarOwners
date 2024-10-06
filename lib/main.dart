517da91f8a2b9a38f4192835e90f6c6edf4f2db6
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-10-06 16:39:21 +0800

    update: main dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
       primarySwatch: Colors.green,
       visualDensity: VisualDensity.adaptivePlatformDensity,
     ),
-    home: const SignupScreen(),
+    home: const LoginScreen(),
   );
 }
 
