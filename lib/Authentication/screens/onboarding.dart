b48f16c5345e0959969315d8e21d34a0dd15e601
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-10-06 18:09:54 +0800

    update: onboarding

--- lib/Authentication/screens/onboarding.dart
+++ lib/Authentication/screens/onboarding.dart
@@ -1,12 +1,13 @@
 import 'package:autocare_carowners/Authentication/screens/onboardingPage1.dart';
 import 'package:autocare_carowners/Authentication/screens/onboardingPage2.dart';
 import 'package:autocare_carowners/Authentication/screens/onboardingPage3.dart';
+import 'package:autocare_carowners/ProfileManagement/models/car_owner_profile_model.dart';
+import 'package:autocare_carowners/ProfileManagement/services/profile_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../ProfileManagement/screens/car_owner_edit_profile.dart';
-
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -19,12 +20,21 @@ class _OnboardingState extends State<Onboarding> {
   final PageController _controller = PageController();
   int currentPageIndex = 0;
   final user = FirebaseAuth.instance.currentUser;
+  CarOwnerProfileModel? profile;
+  bool isLoading = true;  // New state for tracking loading
 
-  get profile => null; // Track the current page index
+  Future<void> _fetchUserProfile() async {
+    final fetchedProfile = await CarOwnerProfileService().fetchUserProfile();
+    setState(() {
+      profile = fetchedProfile;
+      isLoading = false;  // Set loading to false once the profile is fetched
+    });
+  }
 
   @override
   void initState() {
     super.initState();
+    _fetchUserProfile();
     _controller.addListener(() {
       setState(() {
         currentPageIndex = _controller.page?.round() ?? 0; // Update the current page index
@@ -51,26 +61,28 @@ class _OnboardingState extends State<Onboarding> {
               });
             },
             children: [
-              Onboardingpage1(),
-              Onboardingpage2(),
-
-              Onboardingpage3(),
+              const Onboardingpage1(),
+              const Onboardingpage2(),
+              isLoading
+                  ? const Center(child: CircularProgressIndicator())  // Show loading spinner
+                  : CarOwnerEditProfileScreen(currentUser: profile!),  // Only load this when profile is not null
+              const Onboardingpage3(),
             ],
           ),
           Container(
-            alignment: Alignment(0, 0.9),
+            alignment: const Alignment(0, 0.9),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 currentPageIndex == 0
-                    ? Text("      ") // Don't show "pre" if on first page
+                    ? const Text("      ") // Don't show "pre" if on first page
                     : GestureDetector(
                   onTap: () {
                     _controller.previousPage(
-                        duration: Duration(milliseconds: 500),
+                        duration: const Duration(milliseconds: 500),
                         curve: Curves.easeIn);
                   },
-                  child: Icon(Icons.navigate_before),
+                  child: const Icon(Icons.navigate_before),
                 ),
                 SmoothPageIndicator(
                   controller: _controller,
@@ -83,7 +95,7 @@ class _OnboardingState extends State<Onboarding> {
                   ),
                 ),
                 (currentPageIndex == 2 || currentPageIndex == 3)
-                    ? Text("") // Don't show the next icon on the third page or the last page
+                    ? const Text("      ") // Don't show the next icon on the third page or the last page
                     : GestureDetector(
                   onTap: () {
                     _controller.nextPage(
