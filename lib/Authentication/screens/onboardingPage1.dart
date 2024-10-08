448b8b50175cc1fcd7b1921c7a33f786c8b633a7
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-10-09 01:46:42 +0800

    Updated onboardingpage1

--- lib/Authentication/screens/onboardingPage1.dart
+++ lib/Authentication/screens/onboardingPage1.dart
@@ -12,22 +12,31 @@ class _Onboardingpage1State extends State<Onboardingpage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
+      backgroundColor: Colors.orange.shade800,
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                'lib/Authentication/assets/images/onboard1.png',
-                width: 400,
-                height: 400,
+                'lib/Authentication/assets/images/ob2.png',
+                width: 350,
+                height: 350,
                 fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20),
-             const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50.0),
-                child: Text('Experience hassle-free connections to the car services you need!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), // Customize your text style
-                  textAlign: TextAlign.center,),
+              const SizedBox(height: 100),
+              const Positioned(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Text(
+                    'Experience hassle-free connections to the car services you need!',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
             ]
         ), // Fixed closing parenthesis
