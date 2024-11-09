424a0f3acceefb8eb5a24a70c813a82a42decbf3
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-11-09 16:05:52 +0800

    update: gasoline station page

---
+++ lib/Home Page Management/screens/gasoline_station.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../widgets/googleMap.dart';
+
+class GasolineStation extends StatefulWidget {
+  const GasolineStation({super.key});
+
+  @override
+  State<GasolineStation> createState() => _GasolineStationState();
+}
+
+class _GasolineStationState extends State<GasolineStation> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        title: Text('Gasoline Station', style: TextStyle(fontWeight: FontWeight.w900,color: Colors.black),),
+      ),
+      body: Column(
+
+        children: [
+          GoogleMapWidget()
+        ],
+      ),
+    );
+  }
+}
