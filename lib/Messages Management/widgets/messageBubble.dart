735b75beff1043abd313f591aa36814e665091d6
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-11-17 17:36:11 +0800

    Updated message bubble widget

--- lib/Messages Management/widgets/messageBubble.dart
+++ lib/Messages Management/widgets/messageBubble.dart
@@ -1,6 +1,7 @@
 import 'package:autocare_carowners/Messages%20Management/widgets/fullViewImage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/message_model.dart';
 
@@ -12,38 +13,58 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final timeFormat = DateFormat('h:mm a');
+    final messageTime = timeFormat.format(message.timestamp);
+
     return Transform.translate(
-      offset: isMe ? const Offset(-10, 0) : const Offset(10, 0), // Adjust the offset as needed
+      offset: isMe ? const Offset(-10, 0) : const Offset(10, 0),
       child: Align(
         alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 5),
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: isMe ? Colors.orange.shade900 : Colors.white,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: message.imageUrl != null && message.imageUrl!.isNotEmpty
-              ? GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FullImageView(imageUrl: message.imageUrl!),
-                ),
-              );
-            },
-            child: SizedBox(
-              height: 130,
-              child: Image.network(
-                message.imageUrl!,
-                fit: BoxFit.cover,
-              ),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 250),
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            padding: const EdgeInsets.all(7),
+            decoration: BoxDecoration(
+              color: isMe ? Colors.orange.shade900 : Colors.white,
+              borderRadius: BorderRadius.circular(8),
             ),
-          )
-              : Text(
-            message.messageText,
-            style: TextStyle(color: isMe ? Colors.white : Colors.black),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                if (message.imageUrl != null && message.imageUrl!.isNotEmpty)
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FullImageView(imageUrl: message.imageUrl!),
+                        ),
+                      );
+                    },
+                    child: SizedBox(
+                      height: 130,
+                      child: Image.network(
+                        message.imageUrl!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                else
+                  Text(
+                    message.messageText,
+                    style: TextStyle(color: isMe ? Colors.white : Colors.black),
+                  ),
+                const SizedBox(height: 5),
+                Text(
+                  messageTime,
+                  style: TextStyle(
+                    color: isMe ? Colors.white70 : Colors.black54,
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
