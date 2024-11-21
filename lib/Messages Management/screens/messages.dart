33a0597a361d5b574a076450376a17c978d63ce3
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-11-21 14:35:27 +0800

    fix-mid_defense_comments: working

--- lib/Messages Management/screens/messages.dart
+++ lib/Messages Management/screens/messages.dart
@@ -37,7 +37,9 @@ class _CarOwnerMessagesScreenState extends State<CarOwnerMessagesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
         automaticallyImplyLeading: false,
         title: const Text(
           'Messages',
