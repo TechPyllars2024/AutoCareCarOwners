173e9bac2e29fd3f30cff77f8cbe3408e853fc17
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-12-09 20:33:48 +0800

    feat-nearby_services: working nearby feature for gasoline shops gasolineShopWidget.dart

--- lib/Home Page Management/widgets/gasolineShopWidget.dart
+++ lib/Home Page Management/widgets/gasolineShopWidget.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
@@ -18,12 +19,16 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
   final Completer<GoogleMapController> _controller = Completer();
   final List<Marker> _marker = [];
   final Logger logger = Logger();
-  static const IconData localGasStation =
-      IconData(0xe394, fontFamily: 'MaterialIcons');
+  final String assetPath = 'assets/images/gasoline.png';
   bool _isLoading = false;
   bool _hasError = false;
   String _errorMessage = '';
-  final Set<Polyline> _polyline = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeLocationAndFetchStations();
+  }
 
   Future<void> _initializeLocationAndFetchStations() async {
     setState(() {
@@ -66,37 +71,6 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
     controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
   }
 
-  Future<BitmapDescriptor> iconDataToBitmapDescriptor(
-    IconData iconData, {
-    Color color = Colors.blue,
-    double size = 48,
-  }) async {
-    final pictureRecorder = ui.PictureRecorder();
-    final canvas = Canvas(pictureRecorder);
-    final textPainter = TextPainter(
-      textDirection: TextDirection.ltr,
-    );
-
-    // Draw the icon
-    textPainter.text = TextSpan(
-      text: String.fromCharCode(iconData.codePoint),
-      style: TextStyle(
-        fontFamily: iconData.fontFamily,
-        fontSize: size,
-        color: color,
-      ),
-    );
-    textPainter.layout();
-    textPainter.paint(canvas, const Offset(0.0, 0.0));
-
-    // Convert to image and get bytes
-    final picture = pictureRecorder.endRecording();
-    final img = await picture.toImage(size.toInt(), size.toInt());
-    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
-
-    return BitmapDescriptor.fromBytes(byteData!.buffer.asUint8List());
-  }
-
   Future<void> _getNearbyGasStations(double latitude, double longitude) async {
     const apiKey = 'AIzaSyCrbgW2yWOxrm932ZOoVV1_vw1ImfRLMDM';
     final url = Uri.parse(
@@ -109,10 +83,10 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
 
       final results = data['results'];
       if (results != null && results.isNotEmpty) {
-        final customIcon = await iconDataToBitmapDescriptor(
-          localGasStation,
-          color: Colors.orange.shade900,
-          size: 96,
+        final customIcon = await resizeAssetBitmapDescriptor(
+          assetPath,
+          150,
+          150,
         );
 
         for (var result in results) {
@@ -124,6 +98,14 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
           final rating = result['rating'] != null
               ? result['rating'].toString()
               : 'No ratings';
+          final openingHours = result['opening_hours']?['open_now'] == true
+              ? 'Open Now'
+              : 'Closed';
+          final photos = result['photos']?[0]['photo_reference'];
+          final priceLevel = result['price_level'] != null
+              ? 'Price Level: ${result['price_level']}'
+              : 'Price level not available';
+          final businessStatus = result['business_status'] ?? 'Unknown';
 
           setState(() {
             _marker.add(Marker(
@@ -134,7 +116,15 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
                 title: name,
                 snippet: '📍 Address: $address\n⭐ Rating: $rating',
               ),
-              onTap: () => _showNavigationDialog(lat, lng, name),
+              onTap: () => _showDetails(
+                name,
+                rating,
+                address,
+                openingHours,
+                photos,
+                priceLevel,
+                businessStatus,
+              ),
             ));
           });
         }
@@ -150,144 +140,158 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
     }
   }
 
-  void _showNavigationDialog(
-      double destinationLat, double destinationLng, String name) {
-    showDialog(
+  void _showDetails(String name, String rating, String address, String openingHours, String? photos, String priceLevel, String businessStatus) {
+    const apiKey = 'AIzaSyCrbgW2yWOxrm932ZOoVV1_vw1ImfRLMDM';
+    showModalBottomSheet(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Navigate to $name?", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-        content: const Text(
-            "Do you want to start navigation to this gasoline shop?"),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Cancel"),
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              if (photos != null)
+                SizedBox(
+                  height: 120,
+                  width: double.infinity,
+                  child: Image.network(
+                    'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=$photos&key=$apiKey',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const SizedBox(height: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '⭐ Rating: ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange.shade900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: rating,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '📍 Address: ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange.shade900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: address,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '🕒 Status: ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange.shade900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: openingHours,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Price Level: ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange.shade900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: priceLevel,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Business Status: ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange.shade900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: businessStatus,
+                          style: TextStyle(
+                            color: businessStatus == 'OPEN' ? Colors.green : Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              _fetchDirectionsAndNavigate(destinationLat, destinationLng);
-            },
-            child: const Text("Start"),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 
-  List<LatLng> _decodePolyline(String polyline) {
-    List<LatLng> points = [];
-    int index = 0, len = polyline.length;
-    int lat = 0, lng = 0;
+  Future<BitmapDescriptor> resizeAssetBitmapDescriptor(
+      String assetPath, int width, int height) async {
+    // Load the image from assets
+    ByteData data = await rootBundle.load(assetPath);
+    ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+      targetHeight: height,
+    );
+    ui.FrameInfo frameInfo = await codec.getNextFrame();
+    final resizedImage = await frameInfo.image.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
 
-    while (index < len) {
-      int shift = 0, result = 0;
-      int b;
-
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-
-      int deltaLat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += deltaLat;
-
-      shift = 0;
-      result = 0;
-
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-
-      int deltaLng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += deltaLng;
-
-      points.add(LatLng(lat / 1E5, lng / 1E5));
-    }
-
-    return points;
-  }
-
-  Future<void> _fetchDirectionsAndNavigate(
-      double destinationLat, double destinationLng) async {
-    const apiKey = 'AIzaSyCrbgW2yWOxrm932ZOoVV1_vw1ImfRLMDM';
-
-    try {
-      // Step 1: Get user's current location
-      final currentPosition = await _getUserCurrentLocation();
-      if (currentPosition == null) {
-        logger.e("Failed to fetch user's current location.");
-        return;
-      }
-
-      // Step 2: Build API URL
-      final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/directions/json?origin=${currentPosition.latitude},${currentPosition.longitude}&destination=$destinationLat,$destinationLng&key=$apiKey',
-      );
-
-      // Step 3: Fetch directions from API
-      final response = await http.get(url);
-
-      if (response.statusCode == 200) {
-        // Step 4: Parse API response
-        final data = json.decode(response.body);
-
-        final routes = data['routes'];
-        if (routes != null && routes.isNotEmpty) {
-          final polyline = routes[0]['overview_polyline']['points'];
-          if (polyline == null || polyline.isEmpty) {
-            logger.e("No polyline data found in the API response.");
-            return;
-          }
-
-          final points = _decodePolyline(polyline);
-
-          // Step 5: Update UI
-          setState(() {
-            _marker.add(Marker(
-              markerId: const MarkerId("destination"),
-              position: LatLng(destinationLat, destinationLng),
-              infoWindow: const InfoWindow(title: "Destination"),
-            ));
-
-            _polyline.add(
-              Polyline(
-                polylineId: const PolylineId("route"),
-                points: points,
-                color: Colors.blue,
-                width: 5,
-              ),
-            );
-          });
-
-          // Step 6: Start turn-by-turn navigation
-          _startTurnByTurnNavigation(points);
-        } else {
-          logger.e("No routes found in the directions API response.");
-        }
-      } else {
-        logger.e("HTTP error: ${response.statusCode}. Message: ${response.body}");
-      }
-    } catch (e, stackTrace) {
-      logger.e("An error occurred: $e", stackTrace);
-    }
-  }
-
-  void _startTurnByTurnNavigation(List<LatLng> points) async {
-    try {
-      final GoogleMapController controller = await _controller.future;
-
-      for (final point in points) {
-        await Future.delayed(const Duration(seconds: 2));
-        controller.animateCamera(CameraUpdate.newLatLng(point));
-      }
-      logger.i("Navigation complete.");
-    } catch (e, stackTrace) {
-      logger.e("Error during navigation: $e", stackTrace);
-    }
+    // Convert the resized image to BitmapDescriptor
+    return BitmapDescriptor.fromBytes(resizedImage!.buffer.asUint8List());
   }
 
   Future<Position?> _getUserCurrentLocation() async {
@@ -298,12 +302,6 @@ class _GasolineShopsWidgetState extends State<GasolineShopsWidget> {
       logger.i("Error getting location: $e");
       return null;
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeLocationAndFetchStations();
   }
 
   @override
