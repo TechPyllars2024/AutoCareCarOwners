1d1f605bd1bbf43c0dde0684338fc52b6051d2cf
PaulVincentLerado17 <104497476+PaulVincentLerado17@users.noreply.github.com>
2024-12-09 21:01:39 +0800

    update: diagnosis car details fetching

--- lib/Home Page Management/screens/diagnosis.dart
+++ lib/Home Page Management/screens/diagnosis.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
+import '../../Booking Management/services/booking_service.dart';
+import '../widgets/CarDetailsWidget.dart';
 import '../../Booking Management/services/booking_service.dart';
 
 class Diagnosis extends StatefulWidget {
@@ -18,26 +20,41 @@ class _DiagnosisState extends State<Diagnosis> {
   List<dynamic> _currentQuestionsStack = [];
   bool _isAnalyzing = false;
   int _currentStep = 0;
-  bool _isNextButtonEnabled = false; // Track whether Next button is enabled
+  bool _isNextButtonEnabled = false;
   final user = FirebaseAuth.instance.currentUser;
   final Logger logger = Logger();
   Map<String, dynamic>? carDetails;
 
-  // Fetch car details
-  Future<void> fetchCarDetails() async {
+  Future<Map<String, dynamic>> fetchCarDetails() async {
     try {
-      carDetails = await BookingService().fetchDefaultCarDetails(user!.uid);
-      logger.i('Car Owner Data: $carDetails');
+      Map<String, dynamic> fetchedCarDetails =
+      await BookingService().fetchDefaultCarDetails(user!.uid);
+
+      logger.i('Car Owner Data: $fetchedCarDetails');
+      return fetchedCarDetails;
     } catch (e) {
       logger.e('Error fetching car details: $e');
+      return {};
     }
+  }
+
+  void navigateToAddCarDetails() {
+    // Navigate to the "Add Car Details" screen
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Placeholder()),
+    );
   }
 
   @override
   void initState() {
     super.initState();
-    fetchCarDetails(); // Fetch car details once when the widget is initialized
-    readJson(); // Automatically load data when the widget is initialized
+    fetchCarDetails().then((details) {
+      setState(() {
+        carDetails = details;
+      });
+    });
+    readJson();
   }
 
   Future<void> readJson() async {
@@ -46,19 +63,19 @@ class _DiagnosisState extends State<Diagnosis> {
     setState(() {
       _questions = data['diagnosis'] as List<dynamic>;
       _currentQuestionsStack = [_questions];
-      _isAnalyzing = false; // Reset analyzing flag when data is loaded
-      _isNextButtonEnabled = false; // Ensure Next button is disabled initially
+      _isAnalyzing = false;
+      _isNextButtonEnabled = false;
     });
   }
 
   void _selectChoice(int questionIndex, dynamic choice) {
     setState(() {
-      if (_currentQuestions.isEmpty || questionIndex >= _currentQuestions.length) {
-        logger.e("Invalid question index or empty questions list.");
+      if (_currentQuestionsStack.isEmpty || questionIndex >= _currentQuestionsStack.length) {
+        logger.e("Invalid question index or empty questions stack.");
         return;
       }
 
-      var currentQuestion = _currentQuestions[questionIndex];
+      var currentQuestion = _currentQuestionsStack.last[questionIndex];
       if (currentQuestion is! Map || !currentQuestion.containsKey('title')) {
         logger.e("Invalid question format or missing 'title'.");
         return;
@@ -75,15 +92,10 @@ class _DiagnosisState extends State<Diagnosis> {
       );
 
       if (selectedChoiceMap != null && selectedChoiceMap['sub_questions'] != null) {
-        // If sub-questions exist, add them to the stack
         _currentQuestionsStack.add(selectedChoiceMap['sub_questions']);
-        _isAnalyzing = false; // Reset analyzing flag for next question stack
-        _isNextButtonEnabled = false; // Disable Next button when sub-questions are present
+        _isAnalyzing = false;
+        _isNextButtonEnabled = false; // Disable Next button while sub-questions exist
       } else {
-        // If no sub-questions, clear the stack and enable the Next button
-        if (_currentQuestionsStack.isNotEmpty) {
-          _currentQuestionsStack.clear();
-        }
         _isAnalyzing = true;
         _isNextButtonEnabled = true; // Enable Next button if no sub-questions
       }
@@ -95,14 +107,11 @@ class _DiagnosisState extends State<Diagnosis> {
   void _goBack() {
     setState(() {
       if (_currentStep == 0) {
-        // If we are on the car details screen, navigate back
-        Navigator.of(context).pop(); // Pop the current screen to go back
+        Navigator.of(context).pop();
       } else if (_currentQuestionsStack.isNotEmpty) {
-        // If we're not at the very first question, pop the last question set off the stack
         if (_currentQuestionsStack.length > 1) {
           _currentQuestionsStack.removeLast();
         }
-        // Go back one step in the question sequence
         if (_currentStep > 1) {
           _currentStep--;
         }
@@ -120,22 +129,39 @@ class _DiagnosisState extends State<Diagnosis> {
       ),
       body: Column(
         children: [
-          // Display car details before the Stepper
           if (_currentStep == 0)
-            CarDetailsScreen(onNext: () {
-              setState(() {
-                _currentStep = 1; // Move to Step 1 (first question)
-              });
-            })
+            Column(
+              children: [
+                CarDetailsWidget(
+                  carDetailsData: fetchCarDetails(),
+                  navigateToCarDetails: navigateToAddCarDetails,
+                ),
+                // Only show the 'Next' button when car details are fetched
+                if (carDetails != null)
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _currentStep = 1; // Move to stepper
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange.shade900,
+                    ),
+                    child: const Text(
+                      'Next',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+              ],
+            )
           else
-          // Show Stepper with questions after car details are done
             Expanded(
               child: Stepper(
-                currentStep: _currentStep - 1, // Adjusting for Stepper's index (first question on step 1)
+                currentStep: _currentStep - 1,
                 onStepTapped: (int index) {
                   setState(() {
                     if (index >= 0 && index < _currentQuestions.length) {
-                      _currentStep = index + 1; // Adjust to Stepper's step index
+                      _currentStep = index + 1;
                     }
                   });
                 },
@@ -145,7 +171,7 @@ class _DiagnosisState extends State<Diagnosis> {
                       if (_currentStep < _currentQuestions.length) {
                         _currentStep++;
                       } else {
-                        _isAnalyzing = true; // Set analyzing to true when last step is reached
+                        _isAnalyzing = true;
                       }
                     });
                   }
@@ -155,7 +181,7 @@ class _DiagnosisState extends State<Diagnosis> {
                   return Row(
                     children: [
                       ElevatedButton(
-                        onPressed: _isNextButtonEnabled ? details.onStepContinue : null, // Disable if no choice is selected
+                        onPressed: _isNextButtonEnabled ? details.onStepContinue : null,
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.orange.shade900,
                         ),
@@ -214,62 +240,10 @@ class _DiagnosisState extends State<Diagnosis> {
                     : [],
               ),
             ),
-          // Show "Analyzing" message when processing
           if (_isAnalyzing)
             const Center(child: Text('Analyzing the problem')),
         ],
       ),
-    );
-  }
-}
-
-class CarDetailsScreen extends StatelessWidget {
-  final VoidCallback onNext;
-
-  const CarDetailsScreen({required this.onNext, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, dynamic>>(
-      future: BookingService().fetchDefaultCarDetails(FirebaseAuth.instance.currentUser!.uid),
-      builder: (context, snapshot) {
-        // Debugging: print the snapshot's data
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          Map<String, dynamic> carDetails = snapshot.data!;
-
-          // Debugging: print the carDetails to check its structure
-          print('Car Details: $carDetails');
-
-          // Display car details (you can replace with specific fields if needed)
-          return Column(
-            children: [
-              // Displaying all car details temporarily for debugging
-              Text('Car Details: ${carDetails.toString()}'),
-              if (carDetails.containsKey('brand'))
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Text('Brand: ${carDetails['brand']}'),
-                ),
-              ElevatedButton(
-                onPressed: onNext,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange.shade900,
-                ),
-                child: const Text(
-                  'Continue',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          );
-        } else {
-          return const Center(child: Text('No car details found.'));
-        }
-      },
     );
   }
 }
