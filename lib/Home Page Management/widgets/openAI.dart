906a290eaf94287d42ae8dfc97459c9ab5cabc4d
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-12-11 05:17:08 +0800

    feat-diagnosis: working diagnosis screen

--- lib/Home Page Management/widgets/openAI.dart
+++ lib/Home Page Management/widgets/openAI.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 import '../models/request_model.dart';
 import '../models/response_model.dart';
+import 'api_key.dart';
 
 class ChatService {
   static final Uri chatUri = Uri.parse(
@@ -34,8 +35,4 @@ class ChatService {
       return "Error: $e";
     }
   }
-}
-
-  class ApiKey {
-  static const String openAIApiKey = "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";
 }
