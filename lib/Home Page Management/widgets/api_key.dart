a6fd6709170087f632bc4df2453d822332170ead
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-12-11 16:50:12 +0800

    feat-diagnosis: added the google api key in the .gitignore

--- lib/Home Page Management/widgets/api_key.dart
+++ lib/Home Page Management/widgets/api_key.dart
@@ -1,3 +1,4 @@
 class ApiKey {
-  static const String openAIApiKey = "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";
+  static const String openAIApiKey = "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";
+  static const String googleApiKey = "AIzaSyCrbgW2yWOxrm932ZOoVV1_vw1ImfRLMDM";
 }
