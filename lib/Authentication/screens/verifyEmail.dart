7a0b61e176e6212fe12fd098e9e924da927e67a7
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2024-12-12 16:49:13 +0800

    fix: updated the verifyEmail.dart

--- lib/Authentication/screens/verifyEmail.dart
+++ lib/Authentication/screens/verifyEmail.dart
@@ -1,9 +1,8 @@
-import 'package:autocare_carowners/Authentication/screens/onboarding.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-
 import '../Widgets/snackBar.dart';
+import 'onboarding.dart';
 
 class VerifyEmailScreen extends StatefulWidget {
   const VerifyEmailScreen({super.key, this.child});
@@ -32,7 +31,7 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
         // Start periodic check for email verification but don't resend automatically
         timer = Timer.periodic(
           const Duration(seconds: 3),
-          (_) => checkEmailVerified(),
+              (_) => checkEmailVerified(),
         );
       }
     }
@@ -63,20 +62,7 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
 
     try {
       final user = FirebaseAuth.instance.currentUser;
-
-      // Check if user exists and if it's allowed to resend email
       if (user != null && canResendEmail) {
-        // Avoid sending email if it's already verified
-        await user.reload();
-        if (user.emailVerified) {
-          setState(() {
-            isEmailVerified = true;
-          });
-          Utils.showSnackBar("Your email is already verified.");
-          return;
-        }
-
-        // Send verification email
         await user.sendEmailVerification();
 
         setState(() {
@@ -86,11 +72,10 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Verification email sent. Please check your inbox.'),
-            backgroundColor: Colors.green, // Set background color to green
+            backgroundColor: Colors.green,
           ),
         );
 
-        // Cooldown period before allowing another resend
         await Future.delayed(const Duration(seconds: 30));
 
         setState(() {
@@ -99,13 +84,11 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
       }
     } on FirebaseAuthException catch (e) {
       if (e.code == 'too-many-requests') {
-        // Handle rate limit error by disabling the resend button longer
         Utils.showSnackBar("Too many requests. Please try again later.");
         setState(() {
           canResendEmail = false;
         });
 
-        // Wait 60 seconds before allowing resend again
         await Future.delayed(const Duration(seconds: 60));
 
         setState(() {
@@ -128,55 +111,93 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
     return isEmailVerified
         ? const Onboarding()
         : Scaffold(
-            backgroundColor: Colors.grey.shade100,
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              title: const Text(
-                'Verify Email',
-                style: TextStyle(fontWeight: FontWeight.w900),
-              ),
-              backgroundColor: Colors.grey.shade100,
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Verify Email',
+          style: TextStyle(fontWeight: FontWeight.w900),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/Authentication/assets/images/verifyemail.png',
+              width: 200,
+              height: 200,
             ),
-            body: SingleChildScrollView(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'lib/Authentication/assets/images/verifyemail.png',
-                    width: 200,
-                    height: 200,
+            const SizedBox(height: 50),
+            const Column(
+              children: [
+                Text(
+                  'A Verification Email has been Sent!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  const SizedBox(height: 50),
-                  const Text(
-                    'A Verification Email has been sent!',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Kindly check your inbox and spam folder.',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black87,
                   ),
-                  const SizedBox(height: 30),
-                  isLoading
-                      ? const CircularProgressIndicator()
-                      : ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.orange.shade900,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  15), // Change border radius here
-                            ),
-                            minimumSize: const Size.fromHeight(50),
-                          ),
-                          icon: const Icon(Icons.email,
-                              size: 30, color: Colors.white),
-                          label: const Text('Resend Email',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white)),
-                          onPressed:
-                              canResendEmail ? sendVerificationEmail : null,
-                        ),
-                  const SizedBox(height: 8),
-                ],
-              ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-          );
+            const SizedBox(height: 70),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    minimumSize: const Size(400, 45),
+                    backgroundColor: Colors.deepOrange.shade700,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  ),
+                  icon: const Icon(Icons.email, size: 20, color: Colors.white),
+                  label: const Text(
+                    'Resend Email',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  onPressed: canResendEmail && !isLoading ? sendVerificationEmail : null,
+                ),
+                if (isLoading)
+                  Positioned(
+                    child: Container(
+                      width: 350,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange.shade700,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  ),
+
+              ],
+            )
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
